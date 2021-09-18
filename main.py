7643e4bc4d6c40ed7ae36bff32935bfa73b01ff4
EvenEasy <75838378+EvenEasy@users.noreply.github.com>
2021-09-18 21:49:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from os import access
 import config
 import json
 import requests
@@ -16,8 +17,8 @@ temp = myWeather.temperature('celsius')["temp"]
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-with open("data.json", "r") as file:
-    bw = json.load(file)
+with open("data.json", "r", encoding='utf8') as file:
+    data = json.load(file)
 
 def get_news():
     headers = {
@@ -33,22 +34,88 @@ def get_news():
         #desc = article.find("span").text.strip()
         return title
 
+@dp.message_handler(commands=['help'])
+async def info(msg : types.Message):
+    await msg.answer("""Вот список доптупних Команд для усіх : 
+/temp - Температура в м.Дубно
+/news - Крайні новини з сайту Коледжу https://dubnopk.com.ua/index.php/news
+/schedulemon- розклад Пар на Понеділок
+/scheduletue - розклад Пар на Вівторок
+/schedulewed - розклад Пар на Середу
+/schedulethu - розклад Пар на Четвер
+/schedulefri - розклад Пар на П'ятницю
+
+Інші команди доступні тільки для Старости та Заступ. Старости)))
+""")
+
+@dp.message_handler(commands=['SetInfo'])
+async def SetInfo(args : types.Message):
+    a = args.get_args().split(",")
+    if len(a) != 6:
+        await args.answer("Потрібно 6 аргументів")
+        return
+    surname = a[0]
+    KeyInfo = {
+        "surname" : "surname",
+        "full name" : "name",
+        "age" : 15,
+        "status" : None,
+        "num in list" : 1,
+        "phone" : "немає",
+    }
+    arr = list(KeyInfo.keys())
+    for i in range(len(arr)):
+        KeyInfo[arr[i]] = a[i]
+
+    data["StudentsInfo"][surname] = KeyInfo
+    with open("data.json", "w", encoding="utf8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+@dp.message_handler(commands=['GetInfo'])
+async def Getinfo(args : types.Message):
+    answer = ""
+    try:
+        answer += f"___{data['StudentsInfo'][args.get_args()]['surname']}___\n"
+        for key, value in data["StudentsInfo"][args.get_args()].items():
+            answer += f"{key} - {value}\n"
+    except KeyError:
+        answer = "Ім'я не знайдено"
+    await args.answer(answer)
+
+@dp.message_handler(commands=['GetAllInfo'])
+async def GetAllInfo(args : types.Message):
+    for st in list(data["StudentsInfo"].keys()):
+        answer = f"___{data['StudentsInfo'][st]['surname']}___\n"
+        for key, value in data['StudentsInfo'][st].items():
+            answer += f"{key} - {value}\n"
+        await args.answer(answer)
+        answer = ""
+
+@dp.message_handler(commands=["scheduleMon", "scheduleTue", "scheduleWed", "scheduleThu", "scheduleFri"])
+async def Schedule(msgs : types.Message):
+    msg = msgs.text.split('S')[0]
+    arg = msg[-4:-1]
+    try:
+        await msgs.answer('\n'.join(data['Days'][arg].keys()))
+    except KeyError:
+        await msgs.answer("day not found !")
+
 @dp.message_handler(commands=["news"])
 async def news(message : types.Message):
-    await message.answer(f"НОВИНИ    :\n'{get_news()}'\n - https://dubnopk.com.ua/index.php/news")
+    await message.answer(f"НОВИНИ  :\n{get_news()}\n - https://dubnopk.com.ua/index.php/news")
 
-@dp.message_handler(commands=["weather"])
+@dp.message_handler(commands=["temp"])
 async def weather(message : types.Message):
     await message.answer(f"Температура в м.Дубно - {str(temp)}°")
-    if temp < 13:
+    if temp < 10:
         await message.answer("Бажано одягати куртку і штани)))")
 
 @dp.message_handler()
 async def badWord(message : types.Message):
-    for word in bw["badWord"]:
-        if word in (message.text).split(' '):
-            await message.answer("Давай без матів)))")
+    for word in data["BadWords"]:
+        if word.lower() in (message.text.lower()).split(' '):
             await message.delete()
+            await message.answer("Давай без матів)))")
             break
 
 if __name__ == "__main__":
